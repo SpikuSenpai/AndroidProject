b16bf608a947c6dbb5d43eb11887aa214deac77b
Unknown <helenxristos@gmail.com>
2017-11-22 13:57:40 +0200

    MainActivity
    LoginActivity
    RegisterActivity
    AuditActivity

---
+++ app/src/main/java/cutingapp/cuting/org/androidproject/AuditActivity.java
@@ -0,0 +1,13 @@
+package cutingapp.cuting.org.androidproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audit);
+    }
+}
